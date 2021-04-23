a37894834b2889d6497217bc0082ebc9ba8fcf64
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-04-23 12:41:52 +0200

    Added unfinished content

--- src/main/java/dev/thelecrafter/dimensionz/scoreboardmanager/engine/ScoreboardCreator.java
+++ src/main/java/dev/thelecrafter/dimensionz/scoreboardmanager/engine/ScoreboardCreator.java
@@ -34,4 +34,30 @@ public class ScoreboardCreator {
         return scoreboard;
     }
 
+    // Unfinished
+    public static Scoreboard createFromConfigWithTeams() {
+        List<String> lines = FileManager.get().getStringList("lines");
+        String title = FileManager.get().getString("title");
+        return createFromListWithTeams(lines, title);
+    }
+
+    public static Scoreboard createFromListWithTeams(List<String> lines, String title) {
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard scoreboard = manager.getNewScoreboard();
+        Objective objective = scoreboard.registerNewObjective("managerboard", "dummy", title);
+        if (lines.size() > 22) {
+            for (int i = 22; i < lines.size(); i++) {
+                lines.remove(i);
+            }
+            System.out.println("Too many lines!");
+        }
+        for (String line : lines) {
+            if (line.length() > 40) {
+                line = line.substring(0, 40);
+            }
+            // To-Do
+        }
+        return scoreboard;
+    }
+
 }
